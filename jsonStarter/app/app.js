0f219bc7768bc096613575697495ec457f908bf5
Canyon Stockton <canyonj10@gmail.com>
2019-02-19 13:10:01 -0500

    Tier test

--- jsonStarter/app/app.js
+++ jsonStarter/app/app.js
@@ -1,6 +1,19 @@
 function loadData() {
     $.getJSON("data/data.json", function(data) {
-        console.log(data.Nav)
+        //console.log(data.Nav)
+        //for( let i=0; i < data.Nav.length; i++){
+        //    console.log(data.Nav[i].linkName.toLowerCase());
+        //}
+
+        $.each(data.Nav, function(idx, value){
+            let ln = '<a href="' + value.linkURL + '" id="' + value.idName.toLowerCase() + '">' + value.linkName + '</a>';
+            $("nav").append(ln)
+        });
+
+        $.each(data.Section, function(idx, value){
+            let section = '<section id="' + value.idName + '">' + value.headerName + '</section>';
+            $("#content").append(section);
+        })
     });
 }
 
